501e50cef976110e2ddb1345a58e50b402df85f5
Paul Schaefer <yeppaul2@gmail.com>
2017-01-20 09:00:27 -0500

    updated script to match what Tampermonkey wants

--- google-keep/script.js
+++ google-keep/script.js
@@ -1,3 +1,16 @@
-window.onload = function() {
-	document.getElementsByClassName('gb_0b')[0].click();
-};
+// ==UserScript==
+// @name         Google Keep Sidebar Closer
+// @namespace    https://github.com/pschfr/stylish/tree/master/google-keep
+// @version      1.0
+// @description  Closes the sidebar on page load
+// @author       Paul Schaefer
+// @match        keep.google.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+	'use strict';
+	window.onload = function() {
+		document.getElementsByClassName('gb_0b')[0].click();
+	};
+})();
